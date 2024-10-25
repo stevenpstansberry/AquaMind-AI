5b5ffe2bdce34419b21df486de87775b7c9c9a58
Steven <stevenpstansberry@gmail.com>
2024-10-25 12:37:17 -0700

    Fixed CORS error

--- backend/cmd/auth-service/main.go
+++ backend/cmd/auth-service/main.go
@@ -19,7 +19,8 @@ func enableCORS(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Api-Key")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Api-Key, X-Detail-Type")
+
 
         // Handle preflight OPTIONS request
         if r.Method == http.MethodOptions {
