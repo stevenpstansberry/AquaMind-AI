e80954d976f723b853b10d17340dfab969b4b237
Steven Stansberry <stevenpstansberry@gmail.com>
2024-11-04 15:08:28 -0800

    Changed wave color

--- frontend/src/components/home-components/Wave.tsx
+++ frontend/src/components/home-components/Wave.tsx
@@ -10,8 +10,8 @@ const Wave: FC = () => {
   const height = 600;
   let frequency = 0.013;
   const waves = {
-    frontWave: new WaveObj([0.0211, 0.028, 0.015], 'rgb(236, 71, 85, 0.1)'),
-    backWave: new WaveObj([0.0122, 0.018, 0.005], 'rgb(249, 168, 168, 0.1)'),
+    frontWave: new WaveObj([0.0211, 0.028, 0.015], 'rgba(90, 180, 198, 0.40)'), 
+    backWave: new WaveObj([0.0122, 0.018, 0.005], 'rgba(72, 209, 204, 0.40)'), 
   };
 
   const render = () => {
