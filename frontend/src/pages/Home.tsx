9a03c5760bfeed80c2e716ac95ae3b2e6283855b
Steven Stansberry <stevenpstansberry@gmail.com>
2024-11-03 21:16:16 -0800

    Added footer

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -52,7 +52,6 @@ const Home: React.FC = () => {
           <div
             className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
           >
-            <Header />
             <MainHero />
           </div>
         </div>
