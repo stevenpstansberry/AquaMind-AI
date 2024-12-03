0cddffc5fd1459d4c02594699d0d0d6c90741a08
Steven <stevenpstansberry@gmail.com>
2024-12-02 18:52:40 -0800

    Added comments

--- frontend/src/components/aquarium-components/AquariumSidebar.tsx
+++ frontend/src/components/aquarium-components/AquariumSidebar.tsx
@@ -37,7 +37,7 @@ const AquariumSidebar: React.FC<AquariumSidebarProps> = ({
 }) => {
   const theme = useTheme();
 
-  const navbarHeight = 64;
+  const navbarHeight = 64; // Height of the navbar
 
   return (
     <Box
